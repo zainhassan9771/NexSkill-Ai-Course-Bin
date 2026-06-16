950828a285b1c2a778acd4002c6489470a9178fb
zainhassan9771 <zainh9771@gmail.com>
2026-06-16 11:13:26 +0500

    Practice 16 June chocolate

---
+++ Practice chocolate.py
@@ -0,0 +1,8 @@
+ChocolateName = ("Dairy Milk")
+print(ChocolateName)
+ChocolatePrice = 100
+print(ChocolatePrice)
+ChocolateFlavour = ("Almond")
+print(ChocolateFlavour)
+BuyerName = print ("Please enter your name")
+print ("code ends here")
