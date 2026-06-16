844b30445a8911a27dbfe10d7874445c3db0298f
zainhassan9771 <zainh9771@gmail.com>
2026-06-16 10:21:31 +0500

    practice 15 june

---
+++ Case1.py
@@ -0,0 +1,14 @@
+s = ["Geeks", "for", "Geeks"]
+
+print('Starts here')
+
+# using for loop with string
+for i in s:
+    print(i)
+
+print('Mid here')
+
+for i in s:
+    print(i)
+
+print('Close here')
