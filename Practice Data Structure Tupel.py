e558daca8989afce4d1db967570aff621c2ce172
zainhassan9771 <zainh9771@gmail.com>
2026-06-24 18:36:09 +0500

    Data structure Tupel class Practice

---
+++ Practice Data Structure Tupel.py
@@ -0,0 +1,12 @@
+BookTupel=(245,"Think and Gro Rich","Zain",50.5)
+print(BookTupel)
+print(type(BookTupel))
+print(len(BookTupel))
+
+for x in BookTupel:
+    print(x)
+
+print(BookTupel[2])
+print(type(BookTupel[2]))
+print(BookTupel[3])
+print(type(BookTupel[3]))
