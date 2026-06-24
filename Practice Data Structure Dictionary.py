079a3424c78e9fdef206f5c7c59317aff78392de
zainhassan9771 <zainh9771@gmail.com>
2026-06-24 19:02:08 +0500

    Practice Data Type Dictionary

---
+++ Practice Data Structure Dictionary.py
@@ -0,0 +1,8 @@
+BookDictionary={"BookID":245,"BookTitle":"Think and Grow Rich","Author":"Zain","BookPrice":50.5}
+print(BookDictionary)
+print(type(BookDictionary))
+print(len(BookDictionary))
+
+for x in BookDictionary:
+    print(BookDictionary[x])
+    
