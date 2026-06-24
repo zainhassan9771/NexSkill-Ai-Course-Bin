d0c42423b6fda94424e8f4cbbb9f065e91649c4f
zainhassan9771 <zainh9771@gmail.com>
2026-06-24 18:48:03 +0500

    Practice Data structure Set

---
+++ Practice Data Structure Set.py
@@ -0,0 +1,11 @@
+BookSet={245,"Think and Grow Rich","Zain",50.5}
+print(BookSet)
+print(type(BookSet))
+print(len(BookSet))
+
+for x in BookSet:
+    print(x)
+BookSet.add("Ali")
+print(BookSet) 
+BookSet.remove(245)
+print(BookSet)   
