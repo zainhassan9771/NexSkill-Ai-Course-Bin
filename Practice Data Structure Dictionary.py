6c9a681f7e144d36636ce56ba39a8281fb5fb160
Zain Hassan <zainh9771@gmail.com>
2026-09-08 15:55:21 +0500

    Delete Practice Data Structure Dictionary.py

--- Practice Data Structure Dictionary.py
+++
@@ -1,8 +0,0 @@
-BookDictionary={"BookID":245,"BookTitle":"Think and Grow Rich","Author":"Zain","BookPrice":50.5}
-print(BookDictionary)
-print(type(BookDictionary))
-print(len(BookDictionary))
-
-for x in BookDictionary:
-    print(BookDictionary[x])
-    
