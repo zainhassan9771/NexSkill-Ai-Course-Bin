81c5ac5dd8267d5e9473eef3516b1d57ff548871
Zain Hassan <zainh9771@gmail.com>
2026-09-08 15:56:43 +0500

    Delete Practice for 16 june.py

--- Practice for 16 june.py
+++
@@ -1,8 +0,0 @@
-info= "Python is a brilliant programming lnguage"
-print(info)
-print(type(info))
-print(len(info))
-for i in info:
-    print(i)
-    st="Pythonisabrilliantlanguage"
-    print(st[1:15:])
